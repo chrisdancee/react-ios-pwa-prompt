13f5a767e480a449fa972dc6c85ed4ed3198dc61
Chris Dance <chrisjamesdance@gmail.com>
2019-09-26 23:51:01 +0100

    Adds props to configure when and how many times to display

--- src/index.js
+++ src/index.js
@@ -1,97 +1,47 @@
 import React, { Fragment, useEffect, useState } from "react";
 
-import ShareIcon from "./components/ShareIcon";
-import HomeScreenIcon from "./components/HomeScreenIcon";
+import PWAPrompt from "./components/PWAPrompt";
 
-import styles from "./style.scss";
+const deviceCheck = () => {
+  const isiOS = /iphone|ipad|ipod/.test(
+    window.navigator.userAgent.toLowerCase()
+  );
+  const isStandalone =
+    "standalone" in window.navigator && window.navigator.standalone;
 
-export default ({ delay = 1000, title = undefined, copy = undefined }) => {
-  const [isVisible, setVisibility] = useState(!Boolean(delay));
-  const hasBeenDismissed = localStorage.getItem("hasSeenPWAPrompt");
+  return isiOS && !isStandalone;
+};
 
-  useEffect(() => {
-    if (!hasBeenDismissed && delay) {
-      setTimeout(() => setVisibility(true), delay);
-    }
-  }, []);
+export default ({
+  timesToShow = 1,
+  promptOnVisit = 1,
+  title = undefined,
+  copy = undefined,
+  delay = 1000
+}) => {
+  let promptData = JSON.parse(localStorage.getItem("iosPwaPrompt"));
 
-  if (!hasBeenDismissed) {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    const isiOS = () => {
-      return /iphone|ipad|ipod/.test(userAgent);
-    };
-    const isInStandaloneMode = () =>
-      "standalone" in window.navigator && window.navigator.standalone;
+  if (promptData === null) {
+    promptData = { isiOS: deviceCheck(), visits: 0 };
+    localStorage.setItem("iosPwaPrompt", JSON.stringify(promptData));
+  }
 
-    if (isiOS() && !isInStandaloneMode()) {
-      const dismissPrompt = () => {
-        localStorage.setItem("hasSeenPWAPrompt", true);
-        setVisibility(false);
-      };
+  if (promptData.isiOS) {
+    const aboveMinVisits = promptData.visits + 1 >= promptOnVisit;
+    const belowMaxVisits = promptData.visits + 1 < promptOnVisit + timesToShow;
 
-      const isiOS13 = /os 13/.test(userAgent);
-
-      const visibilityClass = isVisible ? styles.visible : styles.hidden;
-      const iOSClass = isiOS13 ? styles.modern : "legacy";
-
-      return (
-        <Fragment>
-          <div
-            className={`${styles.pwaPromptOverlay} ${visibilityClass} ${iOSClass}`}
-            aria-label="Close"
-            role="button"
-            onClick={dismissPrompt}
-          />
-          <div
-            className={`${styles.pwaPrompt} ${visibilityClass} ${iOSClass}`}
-            aria-describedby="pwa-prompt-description"
-            aria-labelledby="pwa-prompt-title"
-            role="dialog"
-          >
-            <div className={styles.pwaPromptHeader}>
-              <p id="pwa-prompt-title" className={styles.pwaPromptTitle}>
-                {title || `Add to Home Screen`}
-              </p>
-              <button
-                className={styles.pwaPromptCancel}
-                onClick={dismissPrompt}
-              >
-                Cancel
-              </button>
-            </div>
-            <div className={styles.pwaPromptBody}>
-              <div className={styles.pwaPromptDescription}>
-                <p id="pwa-prompt-description" className={styles.pwaPromptCopy}>
-                  {copy ||
-                    `This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.`}
-                </p>
-              </div>
-            </div>
-            <div className={styles.pwaPromptInstruction}>
-              <div className={styles.pwaPromptInstructionStep}>
-                <ShareIcon
-                  className={styles.pwaPromptShareIcon}
-                  modern={isiOS13}
-                />
-                <p className={`${styles.pwaPromptCopy} ${styles.bold}`}>
-                  1) Press the 'Share' button
-                </p>
-              </div>
-              <div className={styles.pwaPromptInstructionStep}>
-                <HomeScreenIcon
-                  className={styles.pwaPromptHomeIcon}
-                  modern={isiOS13}
-                />
-                <p className={`${styles.pwaPromptCopy} ${styles.bold}`}>
-                  2) Press 'Add to Home Screen'
-                </p>
-              </div>
-            </div>
-          </div>
-        </Fragment>
+    if (belowMaxVisits) {
+      localStorage.setItem(
+        "iosPwaPrompt",
+        JSON.stringify({
+          ...promptData,
+          visits: promptData.visits + 1
+        })
       );
-    } else {
-      localStorage.setItem("hasSeenPWAPrompt", true);
+
+      if (aboveMinVisits) {
+        return <PWAPrompt delay={delay} title={title} copy={copy} />;
+      }
     }
   }
 
