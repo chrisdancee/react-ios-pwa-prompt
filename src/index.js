6d8b38da61fdef492ad72d96b19c08d110ae2a4e
Konrad Kaliszewski <kkaliszewski@squiz.pl>
2019-10-04 16:43:04 +0200

    Additional props for i18n

--- src/index.js
+++ src/index.js
@@ -3,47 +3,59 @@ import React, { Fragment, useEffect, useState } from "react";
 import PWAPrompt from "./components/PWAPrompt";
 
 const deviceCheck = () => {
-  const isiOS = /iphone|ipad|ipod/.test(
-    window.navigator.userAgent.toLowerCase()
-  );
-  const isStandalone =
-    "standalone" in window.navigator && window.navigator.standalone;
+    const isiOS = /iphone|ipad|ipod/.test(
+        window.navigator.userAgent.toLowerCase()
+    );
+    const isStandalone =
+        "standalone" in window.navigator && window.navigator.standalone;
 
-  return isiOS && !isStandalone;
+    return isiOS && !isStandalone;
 };
 
 export default ({
-  timesToShow = 1,
-  promptOnVisit = 1,
-  title = undefined,
-  copy = undefined,
-  delay = 1000
+    timesToShow = 1,
+    promptOnVisit = 1,
+    title = undefined,
+    copy = undefined,
+    copyAddHomeButtonLabel = undefined,
+    copyShareButtonLabel = undefined,
+    copyClosePrompt = undefined,
+    delay = 1000
 }) => {
-  let promptData = JSON.parse(localStorage.getItem("iosPwaPrompt"));
+    let promptData = JSON.parse(localStorage.getItem("iosPwaPrompt"));
 
-  if (promptData === null) {
-    promptData = { isiOS: deviceCheck(), visits: 0 };
-    localStorage.setItem("iosPwaPrompt", JSON.stringify(promptData));
-  }
-
-  if (promptData.isiOS) {
-    const aboveMinVisits = promptData.visits + 1 >= promptOnVisit;
-    const belowMaxVisits = promptData.visits + 1 < promptOnVisit + timesToShow;
-
-    if (belowMaxVisits) {
-      localStorage.setItem(
-        "iosPwaPrompt",
-        JSON.stringify({
-          ...promptData,
-          visits: promptData.visits + 1
-        })
-      );
-
-      if (aboveMinVisits) {
-        return <PWAPrompt delay={delay} title={title} copy={copy} />;
-      }
+    if (promptData === null) {
+        promptData = { isiOS: deviceCheck(), visits: 0 };
+        localStorage.setItem("iosPwaPrompt", JSON.stringify(promptData));
     }
-  }
 
-  return null;
+    if (promptData.isiOS) {
+        const aboveMinVisits = promptData.visits + 1 >= promptOnVisit;
+        const belowMaxVisits = promptData.visits + 1 < promptOnVisit + timesToShow;
+
+        if (belowMaxVisits) {
+            localStorage.setItem(
+                "iosPwaPrompt",
+                JSON.stringify({
+                    ...promptData,
+                    visits: promptData.visits + 1
+                })
+            );
+
+            if (aboveMinVisits) {
+                return (
+                    <PWAPrompt
+                        delay={delay}
+                        title={title}
+                        copy={copy}
+                        copyAddHomeButtonLabel={copyAddHomeButtonLabel}
+                        copyShareButtonLabel={copyShareButtonLabel}
+                        copyClosePrompt={copyClosePrompt}
+                    />
+                );
+            }
+        }
+    }
+
+    return null;
 };
