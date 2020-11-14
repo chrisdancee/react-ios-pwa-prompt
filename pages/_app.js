d05345f213670caa73cf7a2e72d9d2f08c3d5cc5
Chris Dance <chrisjamesdance@gmail.com>
2020-11-14 16:30:05 +0000

    Adds border to demo pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <style jsx global>{`
+        body {
+          border: 20px solid #e31c5f;
+          height: calc(100vh - 40px);
+          margin: 0;
+          padding: 0;
+          width: calc(100vw - 40px);
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default MyApp;
